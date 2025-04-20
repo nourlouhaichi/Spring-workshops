b82da288486d48af5f94c6b0911561a05d06fc71
nourlouhaichi <nourlouhaichi2@gmail.com>
2025-04-20 21:45:43 +0100

    keywords

--- src/main/java/tn/esprit/tpfoyer/repository/BlocRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repository/BlocRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.tpfoyer.entities.Bloc;
 
+import java.util.List;
+
 @Repository
 public interface BlocRepository extends JpaRepository<Bloc, Long> {
+    List<Bloc> findAllByFoyerIsNull();
+    List<Bloc> findAllByCapaciteBlocGreaterThan(long capacite);
 }
--- src/main/java/tn/esprit/tpfoyer/repository/ChambreRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repository/ChambreRepository.java
@@ -3,8 +3,14 @@ package tn.esprit.tpfoyer.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.tpfoyer.entities.Chambre;
+import tn.esprit.tpfoyer.entities.TypeChambre;
+
+import java.util.List;
 
 @Repository
 public interface ChambreRepository extends JpaRepository <Chambre, Long> {
+
+    List<Chambre> findAllByTypeChambre(TypeChambre tc);
+    Chambre findChambreByNumeroChambre(Long numC);
 
 }
